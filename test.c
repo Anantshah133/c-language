48e7c5891d01115803ab4358a23ce9775342122e
Anantshah133 <anantshah130305@gmail.com>
2024-08-23 18:37:33 +0530

    Leap year with ternary

--- test.c
+++ test.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
 int main(){
-    int n;
+    int n, year = 2024;
 
     printf("Enter N : ");
     scanf("%d", &n);
+
+    year % 4 == 0 ? printf("Leap Year....") : printf("Not Leap year.....");
 
     if(n % 2 == 0){
         printf("Even Number.....");
